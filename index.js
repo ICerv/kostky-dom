ee5cace13ecabdfb93267b22f896f0d25133fa8f
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-23 18:27:48 +0200

    Task completed

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+import { Dice } from "./Dice/index.js";
+
 const roll = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
 
 console.log('funguju!');
+
+const diceRow = document.querySelector('#dice-row');
+diceRow.append(Dice({ side: 2 }));
+diceRow.append(Dice({ side: 1 }));
+diceRow.append(Dice({ side: 4 }));
+diceRow.append(Dice({ side: 5 }));
+diceRow.append(Dice({ side: 6 }));
